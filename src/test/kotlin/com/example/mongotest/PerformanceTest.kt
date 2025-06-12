c1aafc692d9b0ef173adaad204d62456f82f58f0
Fausto Moya <fausto.moya@ah.nl>
2025-06-12 09:40:03 +0200

    refactor PerformanceTest to add repository assertions

--- src/test/kotlin/com/example/mongotest/PerformanceTest.kt
+++ src/test/kotlin/com/example/mongotest/PerformanceTest.kt
@@ -23,7 +23,12 @@ class PerformanceTest {
     private lateinit var mongoTemplate: MongoTemplate
 
     @Autowired
+    private lateinit var repository: TaskRepository
+
+    @Autowired
     private lateinit var taskService: TaskService
+
+    private val faker = Faker()
 
     @BeforeEach
     fun cleanUp() {
@@ -39,31 +44,33 @@ class PerformanceTest {
     fun `bulkInsert takes less than 1 second to save 50_000 documents`() {
         val duration = taskService.bulkInsert(generateTasks(50_000))
         assertThat(duration).isLessThan(Duration.ofSeconds(1))
+        assertThat(repository.count()).isEqualTo(50_000L)
     }
 
     @Test
     fun `saveAll takes more than 15 seconds to save 50_000 documents`() {
         val duration = taskService.saveAll(generateTasks(50_000))
         assertThat(duration).isGreaterThan(Duration.ofSeconds(15))
+        assertThat(repository.count()).isEqualTo(50_000L)
     }
 
     private fun generateTasks(numberOfTasks: Int): List<TaskEntity> {
-        val faker = Faker()
         logger.info("generateData - Start creating $numberOfTasks tasks.")
         val (entities, duration) =
             measureTimedValue {
                 generateSequence { UUID.randomUUID() }
                     .take(numberOfTasks)
-                    .map {
-                        TaskEntity(
-                            it,
-                            action = faker.verb().simplePresent(),
-                            owner = faker.name().fullName(),
-                            product = faker.barcode().gtin12(),
-                        )
-                    }.toList()
+                    .map { aValidTask(it) }
+                    .toList()
             }
         logger.info("generateData - Finished creating $numberOfTasks tasks. Took $duration")
         return entities
     }
+
+    private fun aValidTask(id: UUID) = TaskEntity(
+        id = id,
+        action = faker.verb().simplePresent(),
+        owner = faker.name().fullName(),
+        product = faker.barcode().gtin12(),
+    )
 }
